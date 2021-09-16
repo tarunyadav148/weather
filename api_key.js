5c3797cd2b7277a29232c6f3ef2d98412ab1f3f6
Tarun Singh Yadav <tarunyadav148@gmail.com>
2021-09-16 19:36:37 +0530

    weather website

---
+++ api_key.js
@@ -0,0 +1,1 @@
+const API_KEY = "Your_API_key";
---
+++ script.js
@@ -0,0 +1,120 @@
+const city = document.getElementById("city");
+const currTemp = document.getElementById("curr-temp");
+const icon = document.getElementById("icon");
+const searchbox = document.querySelector(".search-box");
+const description = document.getElementById("description")
+const feelsLike = document.getElementById("feels-like");
+const sunrise = document.getElementById("sunrise");
+const sunset = document.getElementById("sunset");
+const maxTemp = document.getElementById("max-temp");
+const minTemp = document.getElementById("min-temp");
+const humidity = document.getElementById("humidity-data");
+const uvi = document.getElementById("uvi-data");
+const alerts = document.getElementById("alert-data");
+const currLocationbtn = document.getElementById("curr-location");
+
+
+//keyup
+searchbox.addEventListener("keyup", function(event) {
+    if (event.code === 'Enter') {
+        searchEvent()
+    }
+});
+
+//render function
+function getTime(unixTime){
+    date = new Date(unixTime);
+    return date.toLocaleTimeString('en-IN').toUpperCase();
+}
+function render(data){
+    city.innerHTML = data["name"];
+    currTemp.innerHTML = data["main"]["temp"]+ "&#176c";
+    iconCode = data["weather"][0]["icon"];
+    icon.src = `http://openweathermap.org/img/wn/${iconCode}@2x.png`;
+    description.innerHTML = data["weather"][0]["description"][0].toUpperCase()+data["weather"][0]["description"].substr(1);
+    maxTemp.innerHTML = data["main"]["temp_max"]+ "&#176c";
+    minTemp.innerHTML = data["main"]["temp_min"]+ "&#176c";
+    sunrise.innerHTML = getTime(data["sys"]["sunrise"]*1000);
+    sunset.innerHTML  = getTime(data["sys"]["sunset"]*1000);
+    feelsLike.innerHTML = data["main"]["feels_like"] + "&#176c";
+    humidity.innerHTML = data["main"]["humidity"] + "%";
+    setHumidityStat(data["main"]["humidity"]);
+    setVaiOneCallApi(data["coord"]["lat"],data["coord"]["lon"]);
+
+}
+
+//circular stats
+function setHumidityStat(humidity){
+    let humidityStat = document.getElementById("humidity-stat");
+    humidityStat.style.strokeDashoffset = Math.abs((1-(humidity/100))*565);
+}
+function setVaiOneCallApi(lat,lon){
+    let URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${API_KEY}&units=metric`;
+    fetch(URL)
+    .then(res => {
+        res.json()
+        .then(data => {
+            let uviData = data["current"]["uvi"];
+            uvi.innerHTML = uviData;
+            let uviStat = document.getElementById("uvi-stat");
+            uviStat.style.strokeDashoffset = Math.abs(((10-uviData)/10)*565);
+            if(data.hasOwnProperty("alerts"))
+                alerts.innerHTML = data["alerts"][0]["description"];
+            else
+                alerts.innerHTML = "No alert";
+        })
+    })
+    .catch(error => console.log(error()))
+}
+
+//current location button
+currLocationbtn.addEventListener('click',renderByCurrentlaoction);
+
+//on load asked for geolocation
+function renderByCurrentlaoction(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition((position)=>{
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+            fetch(url)
+            .then((res)=>{
+                if(res.ok){
+                    res.json()
+                    .then((data)=>{
+                        render(data);
+                    })
+                }
+            }).catch((error)=> console.log(error))
+        })
+        
+    }
+}
+
+window.addEventListener('load',renderByCurrentlaoction);
+
+//on search
+
+function searchEvent(){
+    let cityName = document.querySelector(".search-box").value;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+    fetch(URL)
+    .then((res)=>{
+        if(res.ok){
+            res.json()
+            .then((data)=>{
+                render(data);
+            })
+        }
+        else{
+            alert("Wrong city name")
+        }
+    })
+    .catch((error)=>console.log(error));
+}
+
+//circular stats
+function setHumidityStat(humidity){
+    let humidityStat = document.getElementById("humidity-stat");
+    humidityStat.style.strokeDashoffset = Math.abs((1-(humidity/100))*565);
+}
